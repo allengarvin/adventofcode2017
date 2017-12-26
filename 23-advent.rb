a8085fa9a283b2c578b11cb249dbcc439669a482
Allen Garvin <aurvondel@gmail.com>
2017-12-25 20:18:40 -0600

    .

--- 23-advent.rb
+++ 23-advent.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby
+# Problem 2 only. Not the one I submitted. I didn't figure what it was doing until about 15 minutes 
+#   after I had reduced it to a series of modulos over the range
 
 require 'prime'
 b=81
-p ((b*100+10**5)..(b*100+10**5+17000)).step(17).select { |n| !Prime.prime?(n) }.length
+p "Answer 2:", ((b*100+10**5)..(b*100+10**5+17000)).step(17).select { |n| !Prime.prime?(n) }.length
 
